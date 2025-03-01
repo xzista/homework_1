e465cb795380484840acf040ae5a2c3b6a1d4e2c
xziste <17alexdmz99@gmail.com>
2025-03-01 12:36:42 +0300

    add func get_date() in module widget

--- src/widget.py
+++ src/widget.py
@@ -13,3 +13,9 @@ def mask_account_card(card: str) -> str:
         return name + get_mask_card_number(int(numbers))
     else:
         return name + get_mask_account(int(numbers))
+
+
+def get_date(date: str) -> str:
+    date_list = date[:10].split("-")
+    date_list.reverse()
+    return ".".join(date_list)
