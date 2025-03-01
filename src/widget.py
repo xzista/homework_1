87a754217468c90edc0347f49334f284d9f6a5a8
xziste <17alexdmz99@gmail.com>
2025-03-01 16:03:30 +0300

    add dockstring in widget.py

--- src/widget.py
+++ src/widget.py
@@ -2,6 +2,8 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card: str) -> str:
+    """Функция, которая умеет обрабатывать информацию как о картах, так и о счетах,
+    и возвращает строку с замаскированным номером"""
     numbers = ""
     name = ""
     for char in card:
@@ -16,6 +18,8 @@ def mask_account_card(card: str) -> str:
 
 
 def get_date(date: str) -> str:
+    """Функция, которая принимает на вход строку с датой в неправильном формате,
+    а возвращает дату в нужном формате"""
     date_list = date[:10].split("-")
     date_list.reverse()
     return ".".join(date_list)
