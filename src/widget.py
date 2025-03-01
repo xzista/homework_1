c6b0c6030e6a5b14a677c621646e83c3937525e8
xziste <17alexdmz99@gmail.com>
2025-03-01 12:06:34 +0300

    add func mask_account_card() in module widget

--- src/widget.py
+++ src/widget.py
@@ -1,2 +1,15 @@
-def mask_account_card():
-    pass
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(card: str) -> str:
+    numbers = ""
+    name = ""
+    for char in card:
+        if char.isdigit():
+            numbers += char
+        else:
+            name += char
+    if len(numbers) == 16:
+        return name + get_mask_card_number(int(numbers))
+    else:
+        return name + get_mask_account(int(numbers))
