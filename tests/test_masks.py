1f752f5763c1e18b80bfb35b68734738395c98eb
xziste <17alexdmz99@gmail.com>
2025-03-16 21:01:30 +0300

    upgrade code in module masks.py and upgrade test_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,19 +1,25 @@
 import pytest
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 @pytest.mark.parametrize('value, result', [(7000792289606361, '7000 79** **** 6361'),
-                                           ('7000792289606361', '7000 79** **** 6361'),
-                                           (792063612896, '7920 63** **** 2896'),
-                                           ('', ' ** **** ')])
+                                           ('7000792289602222', '7000 79** **** 2222')])
 def test_get_mask_card_number(value, result):
     assert get_mask_card_number(value) == result
 
+    with pytest.raises(Exception):
+        get_mask_card_number(7920_6361_289)
+    with pytest.raises(Exception):
+        get_mask_card_number('')
+
 
 @pytest.mark.parametrize('value, result', [(73654108430135874305, '**4305'),
-                                           (430135875410, '**5410'),
-                                           ('73654108430135875410', '**5410'),
-                                           ('', '**')])
+                                           ('84301358758743051111', '**1111')])
 def test_get_mask_account(value, result):
     assert get_mask_account(value) == result
+
+    with pytest.raises(Exception):
+        get_mask_account('')
+    with pytest.raises(Exception):
+        get_mask_account(12323)
