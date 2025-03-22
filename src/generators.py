8d425293420ea52800cc7e21a5c6045639306e36
xziste <17alexdmz99@gmail.com>
2025-03-22 20:24:22 +0300

    add func transaction_descriptions in module generators.py

--- src/generators.py
+++ src/generators.py
@@ -2,3 +2,9 @@ def filter_by_currency(transaction: list[dict], currency: str):
     result = (value for value in transaction if value['operationAmount']['currency']["code"] == currency)
     for val in result:
         yield val
+
+
+def transaction_descriptions(transaction: list[dict]):
+    result = (value["description"] for value in transaction if value.get("description"))
+    for res in result:
+        yield res
