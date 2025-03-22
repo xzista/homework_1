79424652c02ce2dc8eb5bf680f39e28e7123b492
xziste <17alexdmz99@gmail.com>
2025-03-22 20:49:41 +0300

    add func card_number_generator in module generators.py

--- src/generators.py
+++ src/generators.py
@@ -8,3 +8,12 @@ def transaction_descriptions(transaction: list[dict]):
     result = (value["description"] for value in transaction if value.get("description"))
     for res in result:
         yield res
+
+
+def card_number_generator(min_number: int | str, max_number: int | str):
+    i = min_number
+    while i <= max_number:
+        num16 = str(min_number).zfill(16)
+        yield num16[:4] + ' ' + num16[4:8] + ' ' + num16[8:12] + ' ' + num16[-4:]
+        min_number += 1
+        i += 1
