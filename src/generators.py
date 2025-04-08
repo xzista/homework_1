35d556e36cf17d7bece0cc8f55706284c133ed78
xziste <17alexdmz99@gmail.com>
2025-04-08 19:25:32 +0300

    upgrade generators.py and update test_generators.py

--- src/generators.py
+++ src/generators.py
@@ -21,9 +21,7 @@ def transaction_descriptions(transaction: list[dict]):
 def card_number_generator(start: int | str, stop: int | str):
     """Генератор, который генерирует номера карт в заданном диапазоне,
     и выдает номера банковских карт в нужном формате"""
-    i = start
-    while i <= stop:
+    for i in range(start, stop + 1):
         num16 = str(start).zfill(16)
         yield num16[:4] + " " + num16[4:8] + " " + num16[8:12] + " " + num16[-4:]
         start += 1
-        i += 1
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -4,19 +4,17 @@ from src.generators import card_number_generator, filter_by_currency, transactio
 
 
 def test_filter_by_currency(result_data_gen, expected_data_for_gen1):
-    generator1 = filter_by_currency(transaction=result_data_gen, currency="RUS")
-    for gen in generator1:
-        assert next(generator1) == expected_data_for_gen1
-        assert next(generator1) == {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657",
-        }
-        assert next(generator1) == gen
+    generator1 = filter_by_currency(transaction=result_data_gen, currency="RUB")
+    assert next(generator1) == expected_data_for_gen1
+    assert next(generator1) == {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657",
+    }
 
     with pytest.raises(StopIteration):
         generator2 = filter_by_currency(result_data_gen, "")
@@ -47,5 +45,8 @@ def test_transaction_descriptions(result_data_gen):
 def test_card_number_generator(value_min, value_max, result):
     assert next(card_number_generator(value_min, value_max)) == result
 
+    generator = card_number_generator(2, 3)
+    assert next(generator) == "0000 0000 0000 0002"
+    assert next(generator) == "0000 0000 0000 0003"
     with pytest.raises(StopIteration):
-        assert next(card_number_generator(2, 1))
+        next(generator)
