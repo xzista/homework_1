ec8cb625de65a34b0652cd825e8e067e857ff368
xziste <17alexdmz99@gmail.com>
2025-04-08 20:22:24 +0300

    add test_external_api.py and update module external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -3,9 +3,6 @@ import os
 import requests
 from dotenv import load_dotenv
 
-from src.generators import filter_by_currency
-from src.utils import convert_json_to_list_of_dict
-
 
 def transaction_amount(transaction: dict) -> float:
     """Функция, которая принимает на вход транзакцию и
@@ -20,13 +17,4 @@ def transaction_amount(transaction: dict) -> float:
            f'&amount={transaction["operationAmount"]["amount"]}')
     response = requests.request('GET', url=url, headers=headers)
     response_json = response.json()
-    # print(response_json)
-    # print(response.status_code)
-    # print(response.text)
     return round(response_json["result"], 2)
-
-
-if __name__ == '__main__':
-    gen1 = filter_by_currency(convert_json_to_list_of_dict('../data/operations.json'), 'USD')
-    print(transaction_amount(next(gen1)))
-    # print(transaction_amount(next(gen1)))
