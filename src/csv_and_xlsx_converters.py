ce6406ba6d1568734fd1edc2b6c5c168ed64d15a
xziste <17alexdmz99@gmail.com>
2025-04-15 12:49:04 +0300

    update csv_and_xlsx_converters.py after linters

--- src/csv_and_xlsx_converters.py
+++ src/csv_and_xlsx_converters.py
@@ -1,6 +1,7 @@
 import csv
-import pandas as pd
 import logging
+
+import pandas as pd
 
 logger = logging.getLogger("csv_and_xlsx_converters")
 logger.setLevel(logging.DEBUG)
@@ -9,6 +10,7 @@ file_handler = logging.FileHandler("../logs/csv_and_xlsx_converters.log", mode="
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
+
 def convert_csv_to_list_of_dict(path_csv: str) -> list:
     """Функция, которая принимает на вход путь до CSV-файла и
     возвращает список словарей с данными о финансовых транзакциях"""
@@ -16,22 +18,19 @@ def convert_csv_to_list_of_dict(path_csv: str) -> list:
     result = []
     try:
         with open(path_csv, "r", encoding="utf-8") as f:
-            reader = csv.DictReader(f, delimiter=';')
+            reader = csv.DictReader(f, delimiter=";")
             for row in reader:
                 row_dict = {
-                            "id": row["id"],
-                            "state": row["state"],
-                            "date": row["date"],
-                            "operationAmount": {
-                              "amount": row["amount"],
-                              "currency": {
-                                "name": row["currency_name"],
-                                "code": row["currency_code"]
-                              }
-                            },
-                            "description": row["description"],
-                            "from": row["from"],
-                            "to": row["to"]
+                    "id": row["id"],
+                    "state": row["state"],
+                    "date": row["date"],
+                    "operationAmount": {
+                        "amount": row["amount"],
+                        "currency": {"name": row["currency_name"], "code": row["currency_code"]},
+                    },
+                    "description": row["description"],
+                    "from": row["from"],
+                    "to": row["to"],
                 }
                 result.append(row_dict)
     except FileNotFoundError as e:
@@ -48,7 +47,7 @@ def convert_xlsx_to_list_of_dict(path_xlsx: str) -> list:
     logger.info(f"Запуск функции: {convert_xlsx_to_list_of_dict.__name__} с путем к файлу: {path_xlsx}")
     try:
         data_excel = pd.read_excel(path_xlsx)
-        list_excel = data_excel.to_dict(orient='records')
+        list_excel = data_excel.to_dict(orient="records")
     except FileNotFoundError as e:
         logger.error(f"ОШИБКА: {e}")
         print("Ошибка: файл не найден!")
