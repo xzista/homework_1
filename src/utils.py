f9b518ba5950378c325d18a9cc52d25578eade8a
xziste <17alexdmz99@gmail.com>
2025-04-15 05:20:57 +0300

    moved funcs convert_csv_to_list_of_dict() and convert_xlsx_to_list_of_dict() in module csv_and_xlsx_converters.py and add logging

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,3 @@
-import csv
-import pandas as pd
 import json
 import logging
 
@@ -24,51 +22,3 @@ def convert_json_to_list_of_dict(path_json: str) -> list:
         raise e
     logger.info("Функция отработала успешно")
     return result
-
-
-def convert_csv_to_list_of_dict(path_csv: str) -> list:
-    """Функция, которая принимает на вход путь до CSV-файла и
-    возвращает список словарей с данными о финансовых транзакциях"""
-    logger.info(f"Запуск функции: {convert_csv_to_list_of_dict.__name__} с путем к файлу: {path_csv}")
-    result = []
-    try:
-        with open(path_csv, "r", encoding="utf-8") as f:
-            reader = csv.DictReader(f, delimiter=';')
-            for row in reader:
-                row_dict = {
-                            "id": row["id"],
-                            "state": row["state"],
-                            "date": row["date"],
-                            "operationAmount": {
-                              "amount": row["amount"],
-                              "currency": {
-                                "name": row["currency_name"],
-                                "code": row["currency_code"]
-                              }
-                            },
-                            "description": row["description"],
-                            "from": row["from"],
-                            "to": row["to"]
-                }
-                result.append(row_dict)
-    except FileNotFoundError as e:
-        logger.error(f"ОШИБКА: {e}")
-        print("Ошибка: файл не найден!")
-        raise e
-    logger.info("Функция отработала успешно")
-    return result
-
-
-def convert_xlsx_to_list_of_dict(path_xlsx: str) -> list:
-    """Функция, которая принимает на вход путь до xlsx-файла и
-    возвращает список словарей с данными о финансовых транзакциях"""
-    logger.info(f"Запуск функции: {convert_xlsx_to_list_of_dict.__name__} с путем к файлу: {path_xlsx}")
-    try:
-        data_excel = pd.read_excel(path_xlsx)
-        list_excel = data_excel.to_dict(orient='records')
-    except FileNotFoundError as e:
-        logger.error(f"ОШИБКА: {e}")
-        print("Ошибка: файл не найден!")
-        raise e
-    logger.info("Функция отработала успешно")
-    return list_excel
