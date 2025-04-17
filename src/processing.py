192ba47c0ccf17d70812b0e65a67f6d0d12af630
xziste <17alexdmz99@gmail.com>
2025-04-17 19:48:41 +0300

    add func filter_by_description() in module processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+import re
+
+
 def filter_by_state(list_of_dict: list[dict], key_state: str = "EXECUTED") -> list[dict]:
     """Функция, которая принимает список словарей,
     а возвращает новый, отфильтрованный по критерию список словарей"""
@@ -15,3 +18,13 @@ def sort_by_date(list_for_sort: list[dict], reverse_sort: bool = True) -> list[d
     задающий порядок сортировки, а возвращает отсортированный по дате список"""
     sorted_list = sorted(list_for_sort, reverse=reverse_sort, key=lambda i: i["date"])
     return sorted_list
+
+
+def filter_by_description(list_of_dict: list, search: str) -> list:
+    """Функция, которая принимает список словарей и строку поиска,
+    а возвращает список словарей, с искомой строкой поиска в описании"""
+    result =[]
+    for dictionary in list_of_dict:
+        if re.search(f'{search}', dictionary["description"]):
+            result.append(dictionary)
+    return result
